a1ebf5af8874fd7cd0be7a2d0494a5f65ee12b6a
jezzer218 <josh.h218@gmail.com>
2024-12-20 23:37:44 -0600

    Ready to check

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def main ():
     book_path ="books/frankenstein.txt"
     text = get_book_text(book_path)
     words_in_book = word_count(text)
+    
+    char_counted = count_char(text)
+    sorted_list_chars = dict_to_list(char_counted)
+    
+    print(f"--- Begin report of {book_path} ---")
     print(f"{words_in_book} words found in the document")
-    count_char(text)
+    print()
+    
+    for i in sorted_list_chars:
+        if not i["Character"].isalpha():
+            continue
+        print(f"The '{i['Character']}' character was found {i["Count"]} times")
+    
+    print("--- End report ---")
     
         
 def get_book_text(path):
@@ -20,9 +32,23 @@ def word_count(book):
 def count_char(book):
     char_dict = {}
     for char in book:
-        print(char)
-        #if char in char_dict:
-            #char[count + 1]
+        lower_case = char.lower()
+        if lower_case in char_dict:
+            char_dict[lower_case] += 1
+        else:
+            char_dict[lower_case] = 1
+    return char_dict
+
+def sort_on(key):
+    return key["Count"]
+    
+def dict_to_list(char_dict):
+    char_list = []
+    for key in char_dict:
+        char_list.append({"Character": key, "Count": char_dict[key]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+        
     
         
 main()
